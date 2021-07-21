262e196edef3cf6bc2b824760fca8c2ff2e7a077
Sam Lalli <2sam.lalli@gmail.com>
2021-07-21 15:30:43 -0600

    pokemon data rendered in

--- pokedex/pages/index.js
+++ pokedex/pages/index.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout"
 
-export default function Home() {
+export default function Home(pokemon) {
+  console.log(pokemon)
   return (
     <Layout title="NextJs Pokedex">
         <h1 className="text-4xl mb-8 text-center">NextJs Pokedex</h1>
@@ -10,3 +11,23 @@ export default function Home() {
 
 
 
+export async function getStaticProps(context) {
+  try {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+    const {results} = await res.json();
+    const pokemon = results.map((result, index) => {
+      const paddedindex = ("00" + (index + 1)).slice(-3) //returns 001 or 00100
+      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedindex}.png`
+      return {
+        ... result,
+        image,
+
+      }
+    })
+    return{
+      props: { pokemon }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
